6d94b84694c59596da5400e9c51f5de89d394857
I506dk <33561466+I506dk@users.noreply.github.com>
2023-01-05 22:02:34 -0600

    Create install_python.ps1

---
+++ install_python.ps1
@@ -0,0 +1,15 @@
+# Powershell script to install the latest version of python
+    
+$latest_url = (Invoke-RestMethod "https://www.python.org/downloads/") -match "\bhref=(?<url>.+?\.exe)"
+$latest_url = $Matches.url
+$latest_url = $latest_url.replace('"', "")
+
+$latest_version = (Invoke-RestMethod "https://www.python.org/downloads/") -match "Download Python (?<version>\d+\.\d+\.\d+)"
+$latest_version = $Matches.version
+
+Write-Host "Installing Python version:" $latest_version
+$installerPath = Join-Path $env:TEMP (Split-Path $latest_url -Leaf)
+
+Invoke-WebRequest $latest_url -OutFile $installerPath
+Start-Process -FilePath $installerPath -Args "/S" -Verb RunAs -Wait
+Remove-Item $installerPath
