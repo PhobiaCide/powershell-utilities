bbbb57bce34b52c0057febb178d34538cfe5972f
I506dk <33561466+I506dk@users.noreply.github.com>
2023-01-06 14:21:33 -0600

    Create install_dotnet.ps1

---
+++ install_dotnet.ps1
@@ -0,0 +1,7 @@
+# Powershell script to install the dotnet framework 4.8
+$save_path = "$Env:Temp\ndp48-web.exe"
+
+Start-BitsTransfer -Source 'https://go.microsoft.com/fwlink/?linkid=2088631' -Destination $save_path
+Start-Process -FilePath $save_path -Args "/q /norestart /ChainingPackage ADMINDEPLOYMENT" -Verb RunAs -Wait
+
+Remove-Item $save_path
