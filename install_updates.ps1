98393364f56a8f0fb942f5574cc1a61bfd68fd48
I506dk <33561466+I506dk@users.noreply.github.com>
2023-01-06 13:47:41 -0600

    Update install_updates.ps1

--- install_updates.ps1
+++ install_updates.ps1
@@ -1,6 +1,17 @@
 # Powershell script to install the latest windows updates
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-Install-PackageProvider -Name Nuget -Force
+Install-PackageProvider -Name Nuget -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSWindowsUpdate -Force
 Get-Package -Name PSWindowsUpdate
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -ForceDownload -ForceInstall -AutoReboot
+
+Add-WUServiceManager -MicrosoftUpdate
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -ForceDownload -ForceInstall -AutoReboot
+
+$Updates = Get-WindowsUpdate      
+$InstallKB = ($Updates).KB | Select-Object -First 2
+$InstallKB
+Get-WindowsUpdate -KBArticleID $InstallKB -ForceDownload -ForceInstall -Confirm -AcceptAll -IgnoreReboot 
+
+Restart-Computer
