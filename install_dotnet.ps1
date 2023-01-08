4e26b6efbfe96b29e47d4932fc2fbc7c428db0b7
I506dk <33561466+I506dk@users.noreply.github.com>
2023-01-08 15:07:49 -0600

    Update install_dotnet.ps1

--- install_dotnet.ps1
+++ install_dotnet.ps1
@@ -5,3 +5,4 @@ Start-BitsTransfer -Source 'https://go.microsoft.com/fwlink/?linkid=2088631' -De
 Start-Process -FilePath $save_path -Args "/q /norestart /ChainingPackage ADMINDEPLOYMENT" -Verb RunAs -Wait
 
 Remove-Item $save_path
+Restart-Computer
