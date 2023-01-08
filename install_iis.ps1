9a838220676ae3d35dfe565d40c9330f04911193
I506dk <33561466+I506dk@users.noreply.github.com>
2023-01-08 15:34:37 -0600

    Update install_iis.ps1

--- install_iis.ps1
+++ install_iis.ps1
@@ -1,4 +1,4 @@
 # Powershell script to install Internet Information Services (IIS)
 
 # Install iis
-Install-WindowsFeature -name Web-Server -IncludeManagementTools
+Install-WindowsFeature -name Web-Server -IncludeManagementTools -IncludeAllSubFeature
